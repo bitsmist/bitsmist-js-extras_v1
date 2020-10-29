f123fe91beda1878593e0aec9b2ab2c47668e33b
Masaki Yasutake <my17560@gmail.com>
2020-10-29 19:15:09 +0900

    Added route-initializer.

---
+++ src/js/initializer/route-initializer.js
@@ -0,0 +1,105 @@
+// =============================================================================
+/**
+ * BitsmistJS - Javascript Web Client Framework
+ *
+ * @copyright		Masaki Yasutake
+ * @link			https://bitsmist.com/
+ * @license			https://github.com/bitsmist/bitsmist/blob/master/LICENSE
+ */
+// =============================================================================
+
+import { pathToRegexp } from 'path-to-regexp';
+
+// =============================================================================
+//	Route initializer class
+// =============================================================================
+
+export default class RouteInitializer
+{
+
+	// -------------------------------------------------------------------------
+	//  Methods
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Init.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{Object}		settings			Settings.
+	 *
+	 * @return 	{Promise}		Promise.
+	 */
+	static init(component, settings)
+	{
+
+		component._routes = component._routes || [];
+
+		if (settings)
+		{
+			for(let i = 0; i < settings.length; i++)
+			{
+				RouteInitializer.addRoute(component, settings[i]);
+			}
+		}
+
+		return Promise.resolve();
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Check if event is target.
+	 *
+	 * @param	{String}		eventName			Event name.
+	 *
+	 * @return 	{Boolean}		True if it is target.
+	 */
+	static isTarget(eventName)
+	{
+
+		let ret = false;
+
+		if (eventName == "initComponent" || eventName == "connected" || eventName == "spec")
+		{
+			ret = true;
+		}
+
+		return ret;
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	* Add a route.
+	*
+	* @param	{Object}		routeInfo			Route info.
+	* @param	{Boolean}		first				Add to top when true.
+	*/
+	static addRoute(component, routeInfo, first)
+	{
+
+		let keys = [];
+		let route = {
+			"origin": routeInfo["origin"],
+			"name": routeInfo["name"],
+			"path": routeInfo["path"],
+			"keys": keys,
+			"specName": routeInfo["specName"],
+			"componentName": routeInfo["componentName"],
+			"re": pathToRegexp(routeInfo["path"], keys)
+		};
+
+		if (first)
+		{
+			component._routes.unshift(route);
+		}
+		else
+		{
+			component._routes.push(route);
+		}
+
+	}
+
+}
