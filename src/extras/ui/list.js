3e0b994f6f2a35d78ce4833737b696675f40cfe8
Masaki Yasutake <my17560@gmail.com>
2021-02-19 12:28:32 +0900

    Removed a debug log.

--- src/extras/ui/list.js
+++ src/extras/ui/list.js
@@ -96,8 +96,6 @@ Object.defineProperty(List.prototype, 'data', {
 List.prototype.onListAfterAppend = function(sender, e, ex)
 {
 
-	console.log("@@@onListAfterAppend");
-
 	this._listRootNode = this.querySelector(this._settings.get("listRootNode"));
 	let className = ( this._settings.get("components")[this._settings.get("row")]["className"] ? this._settings.get("components")[this._settings.get("row")]["className"] : this._settings.get("row"))
 	this._row = BITSMIST.v1.ClassUtil.createObject(className);
