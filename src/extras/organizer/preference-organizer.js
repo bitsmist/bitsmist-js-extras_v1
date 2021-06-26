c0a04611790f3da217fe7aa39c0ed8d7128cd8dd
Masaki Yasutake <my17560@gmail.com>
2021-06-26 19:39:38 +0900

    Refactored PreferenceOrganizer.

--- src/extras/organizer/preference-organizer.js
+++ src/extras/organizer/preference-organizer.js
@@ -26,18 +26,11 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 	{
 
 		// Init vars
-		PreferenceOrganizer.__preferences = new BITSMIST.v1.Store();
-		PreferenceOrganizer.__observers = new BITSMIST.v1.ObservableStore({"filter":PreferenceOrganizer.__filter});
+		PreferenceOrganizer._store = new BITSMIST.v1.ObservableStore({"filter":PreferenceOrganizer._filter, "async":true});
 		PreferenceOrganizer.__loaded =  {};
 		PreferenceOrganizer.__loaded["promise"] = new Promise((resolve, reject) => {
 			PreferenceOrganizer.__loaded["resolve"] = resolve;
 			PreferenceOrganizer.__loaded["reject"] = reject;
-			setTimeout(() => {
-				reject(`Time out waiting for loading preferences.`);
-			}, 10000);
-		});
-		Object.defineProperty(PreferenceOrganizer, 'preferences', {
-			get() { return PreferenceOrganizer.__preferences; },
 		});
 
 	}
@@ -54,16 +47,8 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 	static init(conditions, component, settings)
 	{
 
-		// Add properties
-		Object.defineProperty(component, 'preferences', {
-			get() { return this._preferences; },
-		});
-
-		// Init vars
-		component._preferences = new PreferenceExporter(component);
-
 		// Register a component as an observer
-		PreferenceOrganizer.__observers.subscribe(component.uniqueId, component.setup.bind(component), {"targets":BITSMIST.v1.Util.safeGet(settings, "preferences.targets")});
+		PreferenceOrganizer._store.subscribe(component.name + "_" + component.uniqueId, PreferenceOrganizer._triggerEvent.bind(component), {"targets":BITSMIST.v1.Util.safeGet(settings, "preferences.targets")});
 
 	}
 
@@ -83,65 +68,47 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 
 		let chain = Promise.resolve();
 
+		// Set default preferences
+		if (component.settings.get("preferences.defaults"))
+		{
+			PreferenceOrganizer._store.items = component.settings.get("preferences.defaults");
+		}
+
+		// Load preferences
 		if (component.settings.get("preferences.load"))
 		{
-			// Set default preferences if any
-			PreferenceOrganizer.__preferences.items = component.settings.get("preferences.defaults");
-
-			// Load preferences
-			chain = PreferenceOrganizer._load(component).then((preferences) => {
+			chain = PreferenceOrganizer.load(component).then((preferences) => {
 				// Merge preferences
-				PreferenceOrganizer.__preferences.merge(preferences);
+				PreferenceOrganizer._store.merge(preferences);
 				PreferenceOrganizer.__loaded.resolve();
 			});
 		}
 
+		// Wait for preference to be loaded
+		let timer;
 		return chain.then(() => {
+			timer = setTimeout(() => {
+				throw new ReferenceError(`Time out waiting for loading preferences. name=${component.name}`);
+			}, 10000);
 			return PreferenceOrganizer.__loaded.promise;
 		}).then(() => {
-			settings["newPreferences"] = PreferenceOrganizer.__preferences.items;
+			clearTimeout(timer);
 			return settings;
 		});
 
 	}
 
 	// -------------------------------------------------------------------------
-	//  Protected
-	// -------------------------------------------------------------------------
 
 	/**
-	* Apply settings.
-	*
-	* @param	{Object}		options				Options.
-	*
-	* @return  {Promise}		Promise.
-	*/
-	static _setup(component, options)
-	{
-
-		options = Object.assign({}, options);
-		let sender = ( options["sender"] ? options["sender"] : this );
-
-		return PreferenceOrganizer.__observers.notify(options).then(() => {
-			if (options["newPreferences"])
-			{
-				PreferenceOrganizer.__preferences.merge(options["newPreferences"]);
-				PreferenceOrganizer._save(component);
-			}
-		});
-
-	}
-
-	// -------------------------------------------------------------------------
-
-	/**
-	* Load items.
-	*
-	* @param	{Object}		options				Options.
-	*
-	* @return  {Promise}		Promise.
-	*/
-	static _load(component, options)
+ 	 * Load preferences.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{Object}		options				Options.
+	 *
+	 * @return  {Promise}		Promise.
+	 */
+	static load(component, options)
 	{
 
 		let sender = ( options && options["sender"] ? options["sender"] : component );
@@ -153,32 +120,49 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 	// -------------------------------------------------------------------------
 
 	/**
-	* Save items.
-	*
-	* @param	{Object}		options				Options.
-	*
-	* @return  {Promise}		Promise.
-	*/
-	static _save(component, options)
+ 	 * Save preferences.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{Object}		options				Options.
+	 *
+	 * @return  {Promise}		Promise.
+	 */
+	static save(component, options)
 	{
 
 		let sender = ( options && options["sender"] ? options["sender"] : component );
 
-		return component.trigger("doSaveStore", sender, {"data":PreferenceOrganizer.__preferences.items});
+		return component.trigger("doSaveStore", sender, {"data":PreferenceOrganizer._store.items});
 
 	}
 
 	// -------------------------------------------------------------------------
-	//  Privates
+	//  Protected
 	// -------------------------------------------------------------------------
 
 	/**
-	* Check if it is a target.
-	*
-	* @param	{Object}		conditions			Conditions.
-	* @param	{Object}		options				Options.
-	*/
-	static __filter(conditions, options)
+ 	 * Load preferences.
+	 *
+	 * @param	{Array}			keys				Changed keys.
+	 *
+	 * @return  {Promise}		Promise.
+	 */
+	static _triggerEvent(keys)
+	{
+
+		return this.trigger("afterPreferenceChange", PreferenceOrganizer, {"keys":keys});
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Check if it is a target.
+	 *
+	 * @param	{Object}		conditions			Conditions.
+	 * @param	{Object}		options				Options.
+	 */
+	static _filter(conditions, options)
 	{
 
 		let result = false;
@@ -186,13 +170,16 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 
 		if (target == "*")
 		{
-			return true;
+			result = true;
 		}
-		else if (target)
+		else
 		{
+			target = ( Array.isArray(target) ? target : [target] );
+			conditions = ( Array.isArray(conditions) ? conditions : [conditions] );
+
 			for (let i = 0; i < target.length; i++)
 			{
-				if (conditions["newPreferences"].hasOwnProperty(target[i]))
+				if (conditions.indexOf(target[i]) > -1)
 				{
 					result = true;
 					break;
@@ -203,22 +190,5 @@ export default class PreferenceOrganizer extends BITSMIST.v1.Organizer
 		return result;
 
 	}
-
-}
-
-// =============================================================================
-//	Preference exporter class
-// =============================================================================
-
-class PreferenceExporter
-{
-
-	constructor(component) { this._component = component; }
-	get items() { return PreferenceOrganizer.__preferences.items; }
-	set(key, value) { return PreferenceOrganizer.__preferences.set(key, value); }
-	get(key) { return PreferenceOrganizer.__preferences.get(key); }
-	load() { return PreferenceOrganizer._load(this._component); }
-	save() { return PreferenceOrganizer._save(this._component); }
-	setup(options) { return PreferenceOrganizer._setup(this._component, options); }
 
 }
