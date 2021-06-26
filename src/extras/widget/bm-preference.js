27f5766da5d250c3582a7bcd87eaf44883418d19
Masaki Yasutake <my17560@gmail.com>
2021-06-26 19:39:58 +0900

    Added bm-preference widget.

---
+++ src/extras/widget/bm-preference.js
@@ -0,0 +1,144 @@
+// =============================================================================
+/**
+ * BitsmistJS - Javascript Web Client Framework
+ *
+ * @copyright		Masaki Yasutake
+ * @link			https://bitsmist.com/
+ * @license			https://github.com/bitsmist/bitsmist/blob/master/LICENSE
+ */
+// =============================================================================
+
+import PreferenceOrganizer from "../organizer/preference-organizer.js";
+
+// =============================================================================
+//	Preference manager class
+// =============================================================================
+
+// -----------------------------------------------------------------------------
+//  Constructor
+// -----------------------------------------------------------------------------
+
+/**
+ * Constructor.
+ */
+export default function PreferenceManager(settings)
+{
+
+	return Reflect.construct(BITSMIST.v1.Pad, [settings], this.constructor);
+
+}
+
+BITSMIST.v1.ClassUtil.inherit(PreferenceManager, BITSMIST.v1.Component);
+
+// -----------------------------------------------------------------------------
+//  Setter/Getter
+// -----------------------------------------------------------------------------
+
+/**
+ * Preference items.
+ *
+ * @type	{Object}
+ */
+Object.defineProperty(PreferenceManager.prototype, "items", {
+	get()
+	{
+		return PreferenceOrganizer._store.items;
+	},
+})
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Get a value from store. Return default value when specified key is not available.
+ *
+ * @param	{String}		key					Key to get.
+ * @param	{Object}		defaultValue		Value returned when key is not found.
+ *
+ * @return  {*}				Value.
+ */
+PreferenceManager.prototype.get = function(key, defaultValue)
+{
+
+	return PreferenceOrganizer._store.get(key, defaultValue);
+
+}
+
+// -------------------------------------------------------------------------
+
+/**
+ * Set a value to the store.
+ *
+ * @param	{String}		key					Key to store.
+ * @param	{Object}		value				Value to store.
+ * @param	{Object}		options				Options.
+ */
+PreferenceManager.prototype.set = function(key, value, options)
+{
+
+	PreferenceOrganizer._store.set(key, value);
+
+	// Save preferences
+	if (BITSMIST.v1.Util.safeGet(options, "autoSave", this.settings.get("preferences.autoSave")))
+	{
+		PreferenceOrganizer.save(this);
+	}
+
+}
+
+// -------------------------------------------------------------------------
+
+/**
+ * Set values to the store.
+ *
+ * @param	{String}		key					Key to store.
+ * @param	{Object}		value				Value to store.
+ * @param	{Object}		options				Options.
+ */
+PreferenceManager.prototype.mergeSet = function(key, value, options)
+{
+
+	PreferenceOrganizer._store.mergeSet(key, value);
+
+	// Save preferences
+	if (BITSMIST.v1.Util.safeGet(options, "autoSave", this.settings.get("preferences.autoSave")))
+	{
+		PreferenceOrganizer.save(this);
+	}
+
+}
+
+// -------------------------------------------------------------------------
+
+/**
+ * Load preferences.
+ *
+ * @param	{Object}		options				Options.
+ *
+ * @return  {Promise}		Promise.
+ */
+PreferenceManager.prototype.load = function(options)
+{
+
+	return PreferenceOrganizer.load(this, options);
+
+}
+
+// -------------------------------------------------------------------------
+
+/**
+ * Save preferences.
+ *
+ * @param	{Object}		options				Options.
+ *
+ * @return  {Promise}		Promise.
+ */
+PreferenceManager.prototype.save = function()
+{
+
+	return PreferenceOrganizer.save(this, options);
+
+}
+
+// -------------------------------------------------------------------------
+
+customElements.define("bm-preference", PreferenceManager);
