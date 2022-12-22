57bb1975beaf0cacb91850d04830693a3e6df065
Masaki Yasutake <my17560@gmail.com>
2022-12-22 14:39:01 +0900

    Added Web storage resource handler.

---
+++ src/extras/resource-handler/webstorage-resource-handler.js
@@ -0,0 +1,118 @@
+// =============================================================================
+/**
+ * BitsmistJS - Javascript Web Client Framework
+ *
+ * @copyright		Masaki Yasutake
+ * @link			https://bitsmist.com/
+ * @license			https://github.com/bitsmist/bitsmist/blob/master/LICENSE
+ */
+// =============================================================================
+
+import ResourceHandler from "./resource-handler.js";
+
+// =============================================================================
+//	Web Storage handler class
+// =============================================================================
+
+export default class WebstorageResourceHandler extends ResourceHandler
+{
+
+	// -------------------------------------------------------------------------
+	//  Constructor
+	// -------------------------------------------------------------------------
+
+	/**
+     * Constructor.
+     *
+	 * @param	{Object}		component			Component.
+     * @param	{String}		resourceName		Resource name.
+     * @param	{Object}		options				Options.
+     */
+	constructor(component, resourceName, options)
+	{
+
+		let defaults = {};
+		super(component, resourceName, Object.assign(defaults, options));
+
+		this._name = "WebstorageResourceHandler";
+
+	}
+
+	// -------------------------------------------------------------------------
+	//  Methods
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Get data.
+	 *
+	 * @param	{String}		id					Target id.
+	 * @param	{Object}		parameters			Query parameters.
+	 *
+	 * @return  {Promise}		Data.
+	 */
+	_get(id, parameters)
+	{
+
+		let data;
+		let rawData = localStorage.getItem(id);
+		if (rawData)
+		{
+			data = JSON.parse(rawData);
+		}
+
+		return data;
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Delete data.
+	 *
+	 * @param	{String}		id					Target id.
+	 * @param	{Object}		parameters			Query parameters.
+	 */
+	_delete(id, parameters)
+	{
+
+		localStorage.removeItem(id);
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Insert data.
+	 *
+	 * @param	{String}		id					Target id.
+	 * @param	{Object}		data				Data to update.
+	 * @param	{Object}		parameters			Query parameters.
+	 */
+	_post(id, data, parameters)
+	{
+
+		localStorage.setItem(id, JSON.stringify(data));
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Update data.
+	 *
+	 * @param	{String}		id					Target id.
+	 * @param	{Object}		data				Data to update.
+	 * @param	{Object}		parameters			Query parameters.
+	 */
+	_put(id, data, parameters)
+	{
+
+		localStorage.setItem(id, JSON.stringify(data));
+
+	}
+
+	// -----------------------------------------------------------------------------
+	//  Privates
+	// -----------------------------------------------------------------------------
+
+}
