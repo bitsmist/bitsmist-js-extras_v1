b0a3fd62fbfcb532c2de615f61ed596a99a38e7e
Masaki Yasutake <my17560@gmail.com>
2023-01-28 13:54:24 +0900

    Modified to throw an error when Bitsmist core does not exist.

--- src/extras/bm.js
+++ src/extras/bm.js
@@ -1,1 +1,6 @@
+if (!window.BITSMIST || !window.BITSMIST.v1 || !window.BITSMIST.v1.Component)
+{
+	throw new ReferenceError("Bitsmist Core Library does not exist.");
+}
+
 export default window.BITSMIST.v1;
