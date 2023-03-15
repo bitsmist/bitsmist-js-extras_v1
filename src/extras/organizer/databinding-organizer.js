27b0432771d8acbe26c156f74bd0980fa3bb51b3
Masaki Yasutake <my17560@gmail.com>
2023-03-15 15:43:30 +0900

    Modified to be able to bind array data.

--- src/extras/organizer/databinding-organizer.js
+++ src/extras/organizer/databinding-organizer.js
@@ -8,6 +8,7 @@
  */
 // =============================================================================
 
+import BindableArrayStore from "../store/bindable-array-store.js";
 import BindableStore from "../store/bindable-store.js";
 import BM from "../bm";
 
@@ -53,18 +54,35 @@ export default class DatabindingOrganizer extends BM.Organizer
 			get() { return this._bindings; },
 		});
 
-		// Add methods
-		component.bindData = function(...args) { return DatabindingOrganizer._bindData(this, ...args); }
+		if (component.settings.get("bindings.settings.type", "single") === "single")
+		{
+			// Add methods
+			component.bindData = function(...args) { return DatabindingOrganizer._bindData(this, ...args); }
 
-		// Init vars
-		component._bindings = new BindableStore({
-			"callback": component.settings.get("bindings.settings.callback"),
-			"type":		component.settings.get("bindings.settings.type", "two-way"),
-		});
+			// Init vars
+			component._bindings = new BindableStore({
+				"direction":	component.settings.get("bindings.settings.direction", "two-way"),
+			});
+
+			// Add event handlers to component
+			this._addOrganizerHandler(component, "afterTransform", DatabindingOrganizer.DatabindingOrganizer_onAfterTransform);
+			this._addOrganizerHandler(component, "doFill", DatabindingOrganizer.DatabindingOrganizer_onDoFill);
+		}
+		else
+		{
+			// Add methods
+			component.bindData = function(...args) { return DatabindingOrganizer._bindDataArray(this, ...args); }
+
+			// Init vars
+			component._bindings = new BindableArrayStore({
+				"direction":	component.settings.get("bindings.settings.direction", "two-way"),
+			});
+
+			// Add event handlers to component
+			this._addOrganizerHandler(component, "doFillRow", DatabindingOrganizer.DatabindingOrganizer_onDoFillRow);
+		}
 
 		// Add event handlers to component
-		this._addOrganizerHandler(component, "afterTransform", DatabindingOrganizer.DatabindingOrganizer_onAfterTransform);
-		this._addOrganizerHandler(component, "doFill", DatabindingOrganizer.DatabindingOrganizer_onDoFill);
 		this._addOrganizerHandler(component, "doCollect", DatabindingOrganizer.DatabindingOrganizer_onDoCollect);
 
 	}
@@ -93,7 +111,17 @@ export default class DatabindingOrganizer extends BM.Organizer
 	}
 
 	// -------------------------------------------------------------------------
-	//
+
+	static DatabindingOrganizer_onDoFillRow(sender, e, ex)
+	{
+
+		DatabindingOrganizer._bindDataArray(this, e.detail.no, e.detail.element, e.detail.callbacks);
+		this._bindings.replace(e.detail.no, e.detail.item);
+
+	}
+
+	// -------------------------------------------------------------------------
+
 	static DatabindingOrganizer_onDoCollect(sender, e, ex)
 	{
 
@@ -128,22 +156,73 @@ export default class DatabindingOrganizer extends BM.Organizer
 		nodes.forEach(elem => {
 			// Get a callback function from settings
 			let key = elem.getAttribute("bm-bind");
-			let callback;
-			component._enumSettings(component.settings.get("bindings"), (sectionName, sectionValue) => {
-				if (sectionValue["callback"])
-				{
-					const pattern = sectionValue["key"] || sectionName;
-					const r = new RegExp(pattern);
-					if (r.test(key))
-					{
-						callback = sectionValue["callback"];
-					}
-				}
-			});
+			let callback = DatabindingOrganizer.__getCallback(component, key);
 
 			// Bind
 			component._bindings.bindTo(key, elem, callback);
 		});
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Bind array data and elements.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{Integer}		index				Array index.
+	 * @param	{HTMLElement}	rootNode			Root node.
+	 */
+	static _bindDataArray(component, index, rootNode)
+	{
+
+		rootNode = ( rootNode ? rootNode : component );
+
+		let nodes = rootNode.querySelectorAll("[bm-bind]");
+		nodes = Array.prototype.slice.call(nodes, 0);
+		if (rootNode.matches("[bm-bind]"))
+		{
+			nodes.push(rootNode);
+		}
+		nodes.forEach(elem => {
+			// Get a callback function from settings
+			let key = elem.getAttribute("bm-bind");
+			let callback = DatabindingOrganizer.__getCallback(component, key);
+
+			// Bind
+			component._bindings.bindTo(index, key, elem, callback);
+		});
+
+	}
+
+	// -------------------------------------------------------------------------
+	//  Protected
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Get a callback function from settings.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{String}		key					Field name.
+	 */
+	static __getCallback(component, key)
+	{
+
+		let callback;
+
+		component._enumSettings(component.settings.get("bindings"), (sectionName, sectionValue) => {
+			if (sectionValue["callback"])
+			{
+				const pattern = sectionValue["key"] || sectionName;
+				const r = new RegExp(pattern);
+				if (r.test(key))
+				{
+					callback = sectionValue["callback"];
+				}
+			}
+		});
+
+		return callback;
 
 	}
 
