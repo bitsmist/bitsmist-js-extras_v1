bc11e194c6059d225e9b27e984a11cfe78ad04d2
Masaki Yasutake <my17560@gmail.com>
2023-05-17 17:18:00 +0900

    Refactored ErrorServer and ErrorPerk.

--- src/extras/component/bm-error.js
+++ src/extras/component/bm-error.js
@@ -15,104 +15,107 @@ import ObservableStore from "../store/observable-store.js";
 //	Error Server class
 // =============================================================================
 
-// -----------------------------------------------------------------------------
-//  Constructor
-// -----------------------------------------------------------------------------
-
-export default function ErrorServer(settings)
+export default class ErrorServer extends BM.Component
 {
 
-	return Reflect.construct(BM.Component, [settings], this.constructor);
+	// -------------------------------------------------------------------------
+	//	Settings
+	// -------------------------------------------------------------------------
 
-}
+	_getSettings()
+	{
 
-BM.ClassUtil.inherit(ErrorServer, BM.Component);
-
-// -----------------------------------------------------------------------------
-//	Settings
-// -----------------------------------------------------------------------------
-
-ErrorServer.prototype._getSettings = function()
-{
-
-	return {
-		"setting": {
-			"autoTransform":			false,
-			"name":						"ErrorServer",
-		},
-		"event": {
-			"events": {
-				"this": {
-					"handlers": {
-						"beforeStart":		["ErrorServer_onBeforeStart"],
+		return {
+			"setting": {
+				"autoTransform":				false,
+			},
+			"event": {
+				"events": {
+					"this": {
+						"handlers": {
+							"beforeStart":		["ErrorServer_onBeforeStart"],
+						}
+					}
+				}
+			},
+			"attendance": {
+				"targets": {
+					"error": {
+						"name": 				"ErrorServer",
 					}
 				}
 			}
-		},
-		"attendance": {
-			"targets": {
-				"error": {
-					"name": 				"ErrorServer",
-				}
+		}
+
+	}
+
+	// -------------------------------------------------------------------------
+	//  Event Handlers
+	// -------------------------------------------------------------------------
+
+	ErrorServer_onBeforeStart(sender, e, ex)
+	{
+
+		this._observers = new BM.ObservableStore({"filter":this.__filter});
+
+		// Install error listner
+		this.__initListeners();
+
+	}
+
+	// -------------------------------------------------------------------------
+	//  Privates
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Filter target components to notify.
+	 *
+	 * @param	{Component}		component			Component.
+	 * @param	{Object}		observerInfo		Observer info.
+	 */
+	__filter(conditions, observerInfo, ...args)
+	{
+
+		let result = false;
+		let targets = observerInfo["options"]["component"].settings.get("errors.targets");
+		let e = args[0]["error"];
+
+		for (let i = 0; i < targets.length; i++)
+		{
+			if (e.name === targets[i] || targets[i] === "*")
+			{
+				result = true;
+				break;
 			}
 		}
+
+		return result;
+
 	}
 
-}
+	// -------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------
-//  Event Handlers
-// -----------------------------------------------------------------------------
-
-ErrorServer.prototype.ErrorServer_onBeforeStart = function(sender, e, ex)
-{
-
-	this._observers = new BM.ObservableStore({"filter":this.__filter});
-
-	// Install error listner
-	this.__initErrorListeners();
-
-}
-
-// -----------------------------------------------------------------------------
-//  Privates
-// -----------------------------------------------------------------------------
-
-/**
- * Filter target components to notify.
- *
- * @param	{Component}		component			Component.
- * @param	{Object}		observerInfo		Observer info.
- */
-ErrorServer.prototype.__filter = function(conditions, observerInfo, ...args)
-{
-
-	let result = false;
-	let targets = observerInfo["options"]["component"].settings.get("errors.targets");
-	let e = args[0]["error"];
-
-	for (let i = 0; i < targets.length; i++)
+	/**
+	 * Init error handling listeners.
+	 */
+	__initListeners()
 	{
-		if (e.name === targets[i] || targets[i] === "*")
-		{
-			result = true;
-			break;
-		}
+
+		window.addEventListener("unhandledrejection", this.__rejectionHandler.bind(this));
+		window.addEventListener("error", this.__errorHandler.bind(this));
+
 	}
 
-	return result;
+	// -------------------------------------------------------------------------
 
-}
+	/**
+	 * Handle unhandled rejection.
+	 *
+	 * @param	{Error}			error				Error object.
+	 */
+	__rejectionHandler(error)
+	{
 
-// -----------------------------------------------------------------------------
-
-/**
- * Init error handling listeners.
- */
-ErrorServer.prototype.__initErrorListeners = function()
-{
-
-	window.addEventListener("unhandledrejection", (error) => {
 		let e = {};
 
 		try
@@ -153,9 +156,22 @@ ErrorServer.prototype.__initErrorListeners = function()
 
 		//return false;
 		return true;
-	});
 
-	window.addEventListener("error", (error, file, line, col) => {
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Handle error.
+	 *
+	 * @param	{Error}			error				Error object.
+	 * @param	{String}		file				File name.
+	 * @param	{Number}		line				Line no.
+	 * @param	{Number}		col					Col no.
+	 */
+	__errorHandler(error, file, line, col)
+	{
+
 		let e = {};
 
 		try
@@ -181,90 +197,92 @@ ErrorServer.prototype.__initErrorListeners = function()
 
 		//return false;
 		return true;
-	});
 
-}
-
-// -----------------------------------------------------------------------------
-
-/**
- * Get an error name for the given error object.
- *
- * @param	{Object}		error				Error object.
- *
- * @return  {String}		Error name.
- */
-ErrorServer.prototype.__getErrorName = function(error)
-{
-
-	let name;
-	let e;
-
-	if (error.reason instanceof XMLHttpRequest)		e = error.reason;
-	else if (error.reason)	e = error.reason;
-	else if (error.error)	e = error.error;
-	else					e = error.message;
-
-	if (e.name)									name = e.name;
-	else if (e instanceof TypeError)			name = "TypeError";
-	else if (e instanceof XMLHttpRequest)		name = "AjaxError";
-	else if (e instanceof EvalError)			name = "EvalError";
-//	else if (e instanceof InternalError)		name = "InternalError";
-	else if (e instanceof RangeError)			name = "RangeError";
-	else if (e instanceof ReferenceError)		name = "ReferenceError";
-	else if (e instanceof SyntaxError)			name = "SyntaxError";
-	else if (e instanceof URIError)				name = "URIError";
-	else
-	{
-		let pos = e.indexOf(":");
-		if (pos > -1)
-		{
-			name = e.substring(0, pos);
-		}
 	}
 
-	return name;
+	// -------------------------------------------------------------------------
 
-}
+	/**
+	 * Get an error name for the given error object.
+	 *
+	 * @param	{Object}		error				Error object.
+	 *
+	 * @return  {String}		Error name.
+	 */
+	__getErrorName(error)
+	{
 
-// -----------------------------------------------------------------------------
+		let name;
+		let e;
 
-/**
- * Handle an exeption.
- *
- * @param	{Object}		e					Error object.
- */
-ErrorServer.prototype.__handleException = function(e)
-{
+		if (error.reason instanceof XMLHttpRequest)		e = error.reason;
+		else if (error.reason)	e = error.reason;
+		else if (error.error)	e = error.error;
+		else					e = error.message;
 
-	console.log("@@@error", this.tagName, e);
-	//window.stop();
-
-	let statusCode = e.object.status;
-	let handlers = this.settings.get("handlers");
-	Object.keys(handlers["statusCode"]).forEach((code) => {
-		if (statusCode == code)
+		if (e.name)									name = e.name;
+		else if (e instanceof TypeError)			name = "TypeError";
+		else if (e instanceof XMLHttpRequest)		name = "AjaxError";
+		else if (e instanceof EvalError)			name = "EvalError";
+	//	else if (e instanceof InternalError)		name = "InternalError";
+		else if (e instanceof RangeError)			name = "RangeError";
+		else if (e instanceof ReferenceError)		name = "ReferenceError";
+		else if (e instanceof SyntaxError)			name = "SyntaxError";
+		else if (e instanceof URIError)				name = "URIError";
+		else
 		{
-			Object.keys(handlers["statusCode"][code]).forEach((command) => {
-				let options = handlers["statusCode"][code][command];
-				switch (command)
-				{
-				case "route":
-					let routeInfo = options["routeInfo"];
-					Object.keys(routeInfo["queryParameters"]).forEach((key) => {
-						routeInfo["queryParameters"][key] = routeInfo["queryParameters"][key].replace("@url@", location.href);
-					});
-					document.querySelector("bm-router").openRoute(routeInfo, {"jump":true});
-					break;
-				}
-			});
+			let pos = e.indexOf(":");
+			if (pos > -1)
+			{
+				name = e.substring(0, pos);
+			}
 		}
-	});
 
-	return this._observers.notifyAsync("error", {"sender":this, "error": e});
+		return name;
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Handle an exeption.
+	 *
+	 * @param	{Object}		e					Error object.
+	 */
+	__handleException(e)
+	{
+
+		//window.stop();
+
+		let statusCode = e.object.status;
+		let handlers = this.settings.get("handlers");
+		Object.keys(handlers["statusCode"]).forEach((code) => {
+			if (statusCode == code)
+			{
+				Object.keys(handlers["statusCode"][code]).forEach((command) => {
+					let options = handlers["statusCode"][code][command];
+					switch (command)
+					{
+					case "route":
+						let routeInfo = options["routeInfo"];
+						Object.keys(routeInfo["queryParameters"]).forEach((key) => {
+							routeInfo["queryParameters"][key] = routeInfo["queryParameters"][key].replace("@url@", location.href);
+						});
+						window.location.href = BM.URLUtil.buildURL(routeInfo, {"jump":true});
+						/*
+						let tagName = options["rootNode"] || "bm-router";
+						document.querySelector(tagName).skills.use("routing.openRoute", routeInfo, {"jump":true});
+						*/
+						break;
+					}
+				});
+			}
+		});
+
+		return this._observers.notifyAsync("error", {"sender":this, "error": e});
+
+	}
 
 }
-
-// -----------------------------------------------------------------------------
 
 customElements.define("bm-error", ErrorServer);
